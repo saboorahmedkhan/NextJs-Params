17eb4957b284df71c0b2effbb0f72b3f725302fa
Saboor Ahmed Khan <saboorahmedkhan@gmail.com>
2024-02-11 01:06:27 +0500

    updated
    updated

---
+++ src/app/[name]/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Home(props:any) {
+    console.log(props)
+    // let params:{name:""}
+    return (
+      <main className="flex justify-center items-center flex-col p-24">
+        <h1 className="text-[36] mb-6">Dynamic Page</h1>
+      </main>
+    );
+  }
+  
